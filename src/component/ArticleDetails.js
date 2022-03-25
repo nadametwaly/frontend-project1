8648602d726dfb60da2021bda789895d9cb9db33
Hager-Abd-El-Galil <81237428+Hager-Abd-El-Galil@users.noreply.github.com>
2022-03-25 17:22:39 +0200

    all

--- src/component/ArticleDetails.js
+++ src/component/ArticleDetails.js
@@ -1,12 +1,18 @@
 import articles from "../utilities/AllArticles"
+import "../styles/ArticleDetails.css";
 import {useParams} from "react-router-dom";
 
 const ArticleDetails = () => {
 const params = useParams();
     return (
-    <div>
-        {articles[params.id - 1].article.title}
+ <div className="AllArticleDetails"> 
+     <br></br> <br></br>      
+    <div className="ArticleDetails">
+       <img src={articles[params.id - 1].article.image}></img>
+       <h2>{articles[params.id - 1].article.title}</h2>
+       <p>{articles[params.id - 1].article.description}</p>
     </div>
+</div>
     );
 };
  
