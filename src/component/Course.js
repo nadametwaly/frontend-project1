1ec54cb52c62fba8a5bedaf7a7150ca653862de0
Mohamed Khattab <Mohamed.E.Khattab.0@gmail.com>
2022-05-08 18:44:42 +0200

    Fetch API response in courses part is done

--- src/component/Course.js
+++ src/component/Course.js
@@ -12,6 +12,20 @@ import React from 'react'
         </div>
         <h2 id="tit">{props.attributes.title}</h2>
         <h3 id="inst">{props.attributes.instructor}</h3>
+        <h3 id="fee"> E£ {props.attributes.fees}</h3>
+        <h1 id="best">BestSeller</h1>
+
+        <div className='ratting'>
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star "></span>
+              <h5 > 4.7</h5>
+              <h6> (490) Student</h6>
+        </div>
+    
       </div>
     );
   };
